17a88331af9483731d437ac3bec85a00057d252d
BOS-boscore <bangkokeos66@gmail.com>
2018-12-06 16:20:07 +0800

    fix bug

--- programs/nodeos/main.cpp
+++ programs/nodeos/main.cpp
@@ -94,6 +94,7 @@ int main(int argc, char** argv)
 {
    try {
       app().set_version(eosio::nodeos::config::version);
+      app.set_http_separate();
       app().register_plugin<history_plugin>();
 
       auto root = fc::app_path();
